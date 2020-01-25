2e6f66afe0f42554dc99c10f2588577b541be586
mohhef <mohhef@rocketmail.com>
2020-01-25 01:27:49 -0500

    added sliderintro componenet

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Slider from './Components/sliderIntro';
+import AppIntroSlider from 'react-native-app-intro-slider';
 
 export default function App() {
 
