c052cd6a8aaebaa8cdfa2308742b07c5450b652f
mohhef <mohhef@rocketmail.com>
2020-01-25 00:50:02 -0500

    sliderIntro component

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Slider from './Components/sliderIntro';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+return(
+  <View>
+    <Slider/>
+  </View>
+)
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
