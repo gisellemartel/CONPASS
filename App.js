b8362fb5f4983746dffd1290df2b2bc3f120429c
mohhef <mohhef@rocketmail.com>
2020-02-20 00:55:50 -0500

    [#8] Add react native google maps

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
-import Home from './src/components/home';
+import MapView from 'react-native-maps';
+import { StyleSheet, View, Dimensions } from 'react-native';
 
 export default class App extends Component {
   render() {
     return (
-      <Home />
+      <View style={styles.container} >
+        <MapView initialRegion={{
+          latitude: 45.492409,
+          longitude: -73.582153,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }} style={styles.mapStyle} />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
