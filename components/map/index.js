4180bdd3f59966b9a4988217ab0185cb3177bd5a
Nichita Hariton <nichitahariton@hotmail.com>
2020-02-22 12:51:26 -0500

    [#9] Add Google Provider for ios

--- components/map/index.js
+++ components/map/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import MapView, {Polygon} from 'react-native-maps';
+import MapView, {Polygon,PROVIDER_GOOGLE} from 'react-native-maps';
 import { StyleSheet, View, Dimensions } from 'react-native';
 import styles from './styles';
 import buildings from 'app/assets/polygons/polygons';
@@ -7,7 +7,9 @@ import buildings from 'app/assets/polygons/polygons';
 export default class TheMap extends Component {
     render() {
         return (    
-                <MapView initialRegion={{
+                <MapView
+                provider={PROVIDER_GOOGLE}
+                 initialRegion={{
                     latitude: 45.492409,
                     longitude: -73.582153,
                     latitudeDelta: 0.0922,
