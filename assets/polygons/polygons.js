f0c26eb1f494f721198cc3c24597cbe4ab07c4eb
Trevor N <trevor.nagg@gmail.com>
2020-02-22 15:04:53 -0500

    [#9] Add polygon delimitation of Communication & Journalism building (Loyola)

--- assets/polygons/polygons.js
+++ assets/polygons/polygons.js
@@ -1,128 +1,174 @@
 // each building is made of many polygons, each polygon will have an array of coordinates
 
-let buildings =
-    [
-        {
-            name: "GM",
-            polygons: [
-                {
-                    name: "GM_building",
-                    coordinates: [
-                        {
-                            latitude: 45.495780,
-                            longitude: -73.579146
-                        },
-                        {
-                            latitude: 45.496134,
-                            longitude: -73.578809
-                        },
-                        {
-                            latitude: 45.495948,
-                            longitude: -73.578435
-                        },
-                        {
-                            latitude: 45.495628,
-                            longitude: -73.578763
-                        }]
-                }
-            ]
-        },
-        {
-            name: "FB",
-            polygons: [
-                {
-                    name: "FB_building",
-                    coordinates: [
-                        {
-                            latitude: 45.494698,
-                            longitude: -73.578035
-                        },
-                        {
-                            latitude: 45.494912,
-                            longitude: -73.577787
-                        },
-                        {
-                            latitude: 45.494654,
-                            longitude: -73.577219
-                        },
-                        {
-                            latitude: 45.494397,
-                            longitude: -73.577520
-                        }]
-                }
-            ]
-        },
-        {
-            name: "Hall",
-            polygons: [
-                {
-                    name: "hall_1",
-                    coordinates: [
-                        {
-                            latitude: 45.497164,
-                            longitude: -73.579548
-                        },
-                        {
-                            latitude: 45.497720,
-                            longitude: -73.579030
-                        },
-                        {
-                            latitude: 45.497352,
-                            longitude: -73.578306
-                        },
-                        {
-                            latitude: 45.496824,
-                            longitude: -73.578850
-                        }]
-                }
-            ]
-        },
-        {
-            name: "EV",
-            polygons: [
-                {
-                    name: "ev_1",
-                    coordinates: [
-                        {
-                            latitude: 45.495599,
-                            longitude: -73.578679
-                        },
-                        {
-                            latitude: 45.495842,
-                            longitude: -73.578451
-                        },
-                        {
-                            latitude: 45.495455,
-                            longitude: -73.577633
-                        },
-                        {
-                            latitude: 45.495225,
-                            longitude: -73.577911
-                        }]
+let buildings = [
+    /*LOYOLA CAMPUS BUILDINGS*/
+    {
+        name: "CJ",
+        polygons: [{
+            name: "Communications & Journalism building",
+            coordinates: [{
+                    latitude: 45.457348,
+                    longitude: -73.640747
                 },
                 {
-                    name: "ev_2",
-                    coordinates: [
-                        {
-                            latitude: 45.495528,
-                            longitude: -73.577544
-                        },
-                        {
-                            latitude: 45.495678,
-                            longitude: -73.578067
-                        },
-                        {
-                            latitude: 45.496047,
-                            longitude: -73.577710,
-                        },
-                        {
-                            latitude: 45.495831,
-                            longitude: -73.577249
-                        },
+                    latitude: 45.457192,
+                    longitude: -73.640441
+                },
+                {
+                    latitude: 45.457392,
+                    longitude: -73.640184
+                },
+                {
+                    latitude: 45.457242,
+                    longitude: -73.640036
+                },
+                {
+                    latitude: 45.457259,
+                    longitude: -73.639829
+                },
+                {
+                    latitude: 45.457418,
+                    longitude: -73.639760
+                },
+                {
+                    latitude: 45.457499,
+                    longitude: -73.639848
+                },
+                {
+                    latitude: 45.457457,
+                    longitude: -73.640058
+                },
+                {
+                    latitude: 45.457493,
+                    longitude: -73.640143
+                },
+                {
+                    latitude: 45.457629,
+                    longitude: -73.640045
+                },
+                {
+                    latitude: 45.457771,
+                    longitude: -73.640389
+                }
 
-                    ]
+            ]
+        }]
+    },
+
+    /* SGW Campus Buildings */
+    {
+        name: "GM",
+        polygons: [{
+            name: "GM_building",
+            coordinates: [{
+                    latitude: 45.495780,
+                    longitude: -73.579146
+                },
+                {
+                    latitude: 45.496134,
+                    longitude: -73.578809
+                },
+                {
+                    latitude: 45.495948,
+                    longitude: -73.578435
+                },
+                {
+                    latitude: 45.495628,
+                    longitude: -73.578763
                 }
             ]
-        },
-    ]
+        }]
+    },
+    {
+        name: "FB",
+        polygons: [{
+            name: "FB_building",
+            coordinates: [{
+                    latitude: 45.494698,
+                    longitude: -73.578035
+                },
+                {
+                    latitude: 45.494912,
+                    longitude: -73.577787
+                },
+                {
+                    latitude: 45.494654,
+                    longitude: -73.577219
+                },
+                {
+                    latitude: 45.494397,
+                    longitude: -73.577520
+                }
+            ]
+        }]
+    },
+    {
+        name: "Hall",
+        polygons: [{
+            name: "hall_1",
+            coordinates: [{
+                    latitude: 45.497164,
+                    longitude: -73.579548
+                },
+                {
+                    latitude: 45.497720,
+                    longitude: -73.579030
+                },
+                {
+                    latitude: 45.497352,
+                    longitude: -73.578306
+                },
+                {
+                    latitude: 45.496824,
+                    longitude: -73.578850
+                }
+            ]
+        }]
+    },
+    {
+        name: "EV",
+        polygons: [{
+                name: "ev_1",
+                coordinates: [{
+                        latitude: 45.495599,
+                        longitude: -73.578679
+                    },
+                    {
+                        latitude: 45.495842,
+                        longitude: -73.578451
+                    },
+                    {
+                        latitude: 45.495455,
+                        longitude: -73.577633
+                    },
+                    {
+                        latitude: 45.495225,
+                        longitude: -73.577911
+                    }
+                ]
+            },
+            {
+                name: "ev_2",
+                coordinates: [{
+                        latitude: 45.495528,
+                        longitude: -73.577544
+                    },
+                    {
+                        latitude: 45.495678,
+                        longitude: -73.578067
+                    },
+                    {
+                        latitude: 45.496047,
+                        longitude: -73.577710,
+                    },
+                    {
+                        latitude: 45.495831,
+                        longitude: -73.577249
+                    },
+
+                ]
+            }
+        ]
+    },
+]
 export default buildings;
