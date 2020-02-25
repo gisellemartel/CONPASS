62673eeca5c00a7b5a1b31e3842b55d06d0007ae
Trevor N <trevor.nagg@gmail.com>
2020-02-25 10:55:17 -0500

    [#9] Run eslint to standardize code

--- components/searchBar/styles.js
+++ components/searchBar/styles.js
@@ -1,58 +1,31 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'white',
-        position: 'absolute',
-        top: 25,
-        width: '95%',
-        borderRadius: 10
-    },
-    view: {
-        borderRadius: 15
-    },
-    Bar: {
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    Touch: {
-        color: '#AFB9C4',
-        borderWidth: 1,
-        padding: 10,
-        margin: 1,
-        borderRadius: 5,
-
-    },
-    burger: {
-        resizeMode: 'contain',
-        width: 25,
-        height: 250,
-        margin: 10
-    },
-
-    sug: {
-        backgroundColor: '#F1F7FC',
-        height: '100%',
-    },
-
-    suggestions: {
-        color: 'black',
-        padding: 5,
-        width: '90%',
-        borderWidth: 0.5,
-        marginLeft: 5,
-        marginRight: 5,
-    },
-    searchBar: {
-        color: 'white',
-        borderWidth: 0.5,
-        width: '95%',
-        marginLeft: 5,
-        marginRight: 5
-    }
+  container: {
+    backgroundColor: 'white',
+    position: 'absolute',
+    top: 25,
+    width: '95%',
+    height: 40,
+    borderRadius: 10
+  },
+  searchBar: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  burger: {
+    resizeMode: 'contain',
+    width: 25,
+    height: 250,
+    margin: 10
+  },
+  searchBarText: {
+    fontSize: 20,
+    color: 'grey'
+  },
 });
 
 export default styles;
