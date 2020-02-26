2da16deef2ffd91dc09b008e33d4654958771604
sebhani <hani-111-222@hotmail.com>
2020-02-26 14:17:04 -0500

    [#10] Add the functionality of switching between campuses

--- components/switchCampuses/index.js
+++ components/switchCampuses/index.js
@@ -10,15 +10,48 @@ export default class SwitchCampuses extends Component {
         super(props);
     }
 
+    async getLatLong(prediction) {
+        // eslint-disable-next-line react/no-unused-state
+        this.setState({ description: prediction });
+        const key = 'AIzaSyCqNODizSqMIWbKbO8Iq3VWdBcK846n_3w';
+        const geoUrl = `https://maps.googleapis.com/maps/api/place/details/json?key=${key}&placeid=${prediction}`;
+
+        try {
+            const georesult = await fetch(geoUrl);
+            const gjson = await georesult.json();
+            this.setState({ locations: gjson.result.geometry.location });
+
+            this.setState({
+                region: {
+                    // eslint-disable-next-line react/no-access-state-in-setstate
+                    latitude: this.state.locations.lat,
+                    // eslint-disable-next-line react/no-access-state-in-setstate
+                    longitude: this.state.locations.lng,
+                    latitudeDelta: 0.05,
+                    longitudeDelta: 0.05
+                }
+
+            });
+            this.props.callBack(this.state.region);
+        } catch (err) {
+            console.error(err);
+        }
+    }
 
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.btn}>
-                    <Button title='Layola' />
+                    <Button title='Layola'
+                        onPress={() => this.getLatLong('ChIJJ4gBMS4XyUwR5Cxm6Yq7mhc')}
+                    />
+
                 </View>
+
                 <View style={styles.btn}>
-                    <Button title='SGW' />
+                    <Button title='SGW'
+                        onPress={() => this.getLatLong('ChIJCT3qZGoayUwRmPk37VHZSRY')}
+                    />
                 </View>
             </View>
         );
