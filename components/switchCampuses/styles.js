0534d2f671f131ed3c0bdf3e0c7c0be98845bc77
mohhef <mohhef@rocketmail.com>
2020-02-26 15:50:28 -0500

    [#10] Replace getLangLong function & Put styles in seprate file

---
+++ components/switchCampuses/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  btn: {
+    bottom: '97%',
+    width: '30%',
+    marginHorizontal: 5,
+  },
+  container: {
+    flexDirection: 'row',
+    position: 'absolute',
+    justifyContent: 'center'
+  },
+});
+
+export default styles;
