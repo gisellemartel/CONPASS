d65e855c9c229b60305eda4a4c1b68dabd4e6a9d
Trevor N <trevor.nagg@gmail.com>
2020-02-28 16:07:25 -0500

    [#18] Set up properties in home and map to enable passing of data from shuttle component

--- components/home/index.js
+++ components/home/index.js
@@ -42,17 +42,30 @@ class Home extends Component {
     });
   }
 
+  getPolylinePoint = (data) => {
+    this.setState({
+      encryptedLine: data
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <TheMap updatedRegion={this.state.region} updatedCoordinates={this.state.coordinates}/>
+        <TheMap
+          updatedRegion={this.state.region}
+          updatedCoordinates={this.state.coordinates}
+          encryptedLine={this.state.encryptedLine}
+        />
         <SearchBar
           navigation={this.props.navigation}
           updateRegion={this.updateRegion}
           changeVisibilityTo={this.changeVisibilityTo}
         />
         <SwitchCampuses updateRegion={this.updateRegion} visiblityState={this.state.isVisible} />
-        <Shuttle coordinateCallback={this.updateCoordinates} />
+        <Shuttle
+          coordinateCallback={this.updateCoordinates}
+          getPolylinePoint={this.getPolylinePoint}
+        />
       </View>
     );
   }
--- components/map/index.js
+++ components/map/index.js
@@ -1,26 +1,20 @@
+/* eslint-disable class-methods-use-this */
 /* eslint-disable react/no-unused-state */
 import React, { Component } from 'react';
 import MapView, { Polygon, Polyline, PROVIDER_GOOGLE } from 'react-native-maps';
 
-import decodePolyline from 'decode-google-map-polyline';
 
 export default class TheMap extends Component {
   constructor(props) {
     super(props);
-    const rawPolylinePoints = decodePolyline('qsmtGnrm`MuA|Eu@jC}@m@u@a@Sh@eAy@uA{@eGqEyG{EeHeFeFcEmDqCgBaBuI_IaCuBiCkBo@e@yAgA{KsJwGqFoF}EoFsE{EqEq@i@aAo@SAc@@[JMi@c@oA]q@c@k@gBuAk@c@KIPe@l@wAx@qAl@kAt@eAlAoAdByArBoBPU\\o@dBsBnAsBzDwHfCiFhBoE`BwEdCiHnDsJ@c@n@mBfDyJlFgPjAsDlAsELwALmD?}@AaAKaAE]OeAc@kBc@wAY}@[s@}@oA{@u@_@]YUk@s@sA{@u@[oAc@a@IKAUBUIgBe@kCs@kDcA{DiAmBs@{A{@qA{@}@s@uBuBiAwAmA{BeCuE_EiGsCkFmF_KkIwOoAwBcAcBYc@?SE[aCqEoA{ByAsCWa@a@_@w@c@gAe@]Uw@eAwDsF_CwCg@a@SKYGOA_@?a@FQFe@Rc@Z[\\SVsBgBiF_FoDiDqCiCoCiCKXcAbCcCxGyBnFiA~C');
-    // Incompatible field names for direct decode. Need to do a small conversion.
-    const waypoints = rawPolylinePoints.map((point) => {
-      return {
-        latitude: point.lat,
-        longitude: point.lng
-      };
-    });
+    const waypoints = '';
     this.state = {
       region: {
         latitude: 45.492409,
         longitude: -73.582153
       },
-      coordinates: waypoints
+      coordinates: waypoints,
+      encryptedLine: ''
     };
   }
 
@@ -38,7 +32,7 @@ export default class TheMap extends Component {
           style={styles.mapStyle}
         >
           <Polyline
-            coordinates={this.state.coordinates}
+            coordinates={this.props.updatedCoordinates}
             strokeWidth={4}
             strokeColor="black"
           />
@@ -57,6 +51,7 @@ export default class TheMap extends Component {
           );
         })}
         </MapView>
+
       </View>
     );
   }
