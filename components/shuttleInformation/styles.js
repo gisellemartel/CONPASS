8a0c174f1152f69c124eed481839c3420d980c81
Trevor N <trevor.nagg@gmail.com>
2020-02-28 16:03:33 -0500

    [#18] Restyle the links on the bottom of the page

--- components/shuttleInformation/styles.js
+++ components/shuttleInformation/styles.js
@@ -1,16 +1,11 @@
-import { StyleSheet, Dimensions } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-  mapStyle: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
+
+  }
 });
 
 export default styles;
