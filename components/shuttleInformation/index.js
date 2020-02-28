1ff2c828b421647e7702ce032764b7c58a7a1202
Trevor N <trevor.nagg@gmail.com>
2020-02-28 16:09:28 -0500

    [#18] Add polyline from current location to either SGW shuttle stop or LOY shuttle stop

--- components/shuttleInformation/index.js
+++ components/shuttleInformation/index.js
@@ -1,16 +1,52 @@
+/* eslint-disable react/no-deprecated */
+/* eslint-disable react/no-unused-state */
 import React, { Component } from 'react';
-import { View, Button } from 'react-native';
+import { View, Button, Alert } from 'react-native';
 import decodePolyline from 'decode-google-map-polyline';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+import shuttleServiceInformation from './shuttleService';
 import styles from './styles';
 
 
 export default class Shuttle extends Component {
-  async getDirections(origin, destination) {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: '',
+      errorMessage: null
+    };
+    this.getCurrentLocation();
+  }
+
+  async getDirectionsToShuttleBusStop(destination) {
+    await this.getCurrentLocation();
+    const { location } = this.state;
+    let destCoordinates = [];
+    if (destination === 'SGW') { destCoordinates = [shuttleServiceInformation[0].latitude, shuttleServiceInformation[0].longitude]; } else if (destination === 'LOY') { destCoordinates = [shuttleServiceInformation[1].latitude, shuttleServiceInformation[1].longitude]; }
+    await this.drawPath([location.coords.latitude, location.coords.longitude], destCoordinates);
+  }
+
+  async getCurrentLocation() {
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+    const location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+  }
+
+  async drawPath(origin, destination) {
     const key = 'AIzaSyBsMjuj6q76Vcna8G5z9PDyTH2z16fNPDk';
     const directionUrl = `https://maps.googleapis.com/maps/api/directions/json?key=${key}&origin=${origin[0]},${origin[1]}&destination=${destination[0]},${destination[1]}`;
     try {
       const result = await fetch(directionUrl);
       const json = await result.json();
+      const encryptedPath = json.routes[0].overview_polyline.points;
+      const { getPolylinePoint } = this.props;
+      getPolylinePoint(encryptedPath);
       const rawPolylinePoints = decodePolyline(json.routes[0].overview_polyline.points);
       // Incompatible field names for direct decode. Need to do a trivial conversion.
       const waypoints = rawPolylinePoints.map((point) => {
@@ -26,6 +62,7 @@ export default class Shuttle extends Component {
     }
   }
 
+
   render() {
     return (
       <View style={styles.container}>
@@ -37,9 +74,14 @@ export default class Shuttle extends Component {
           <Button
             title="Get Shuttle Bus Directions"
             onPress={() => {
-              this.getDirections(
-                [45.459026, -73.638606],
-                [45.497050, -73.578009]
+              Alert.alert(
+                'Select the Campus',
+                'Which campus would you like to get directions to the Shuttle Bus Stop?',
+                [
+                  { text: 'SGW', onPress: () => { this.getDirectionsToShuttleBusStop('SGW'); } },
+                  { text: 'LOY', onPress: () => { this.getDirectionsToShuttleBusStop('LOY'); } },
+                ],
+                { cancelable: false }
               );
             }}
           />
