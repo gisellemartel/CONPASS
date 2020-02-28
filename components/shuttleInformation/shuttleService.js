ae39e4746f733dad8dcf64297f4a01e10cd87faa
Trevor N <trevor.nagg@gmail.com>
2020-02-28 16:04:38 -0500

    [#18] Add shuttle bus stop information

---
+++ components/shuttleInformation/shuttleService.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+const shuttleServiceInformation = [
+  {
+    campus: 'SGW',
+    address: '1455 De Maisonneuve Blvd. W.',
+    latitude: 45.497029,
+    longitude: -73.578647
+  },
+  {
+    campus: 'LOY',
+    address: '7137 Sherbrooke St. W.',
+    latitude: 45.458423,
+    longitude: -73.638301
+  }
+];
+export default shuttleServiceInformation;
