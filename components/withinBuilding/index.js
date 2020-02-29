89b125cd7a134a909d4e7ca9c690fd5e802fb451
sebhani <hani-111-222@hotmail.com>
2020-02-29 06:32:08 -0500

    [#11] Add SGW polygon in which all buildings in the campus are included

--- components/withinBuilding/index.js
+++ components/withinBuilding/index.js
@@ -2,22 +2,30 @@ import React, { Component } from 'react';
 import {
   View, Keyboard, TouchableOpacity, Text
 } from 'react-native';
-import { render } from 'react-dom';
-import GeoFencing from 'react-native-geo-fencing';
+import buildings from '../../assets/polygons/polygons';
 
 export default class WithinBuilding extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            //polygon covering all buildings in SGW
+            xSGWCoordinates: [-73.5866750, -73.5764890, -73.5715300, -73.5814433],
+            ySGWCoordinates: [45.4963950, 45.4912630, 45.4961810, 45.5008758],
+            
+            //polygon covering all buildings in Loyola
+
+            //polygon covering individual buildings taken from polygons.js file
             xCoordinates: [-73.578062,-73.578638, -73.577702, -73.577063],
             yCoordinates: [45.497284,45.496698, 45.496227, 45.496862],
+            
+            //user's location 
             xPoint: -73.58906809999999,
             yPoint: 45.4969778,
         }
     }
 
-  
+
     pnpoly(nvert, vertx, verty, testx, testy)
     {
         var i, j, c = 0;
@@ -30,8 +38,7 @@ export default class WithinBuilding extends Component {
     }
 
   render(){
-      //this.state
-
+    
     this.pnpoly(4, this.state.xCoordinates, this.state.yCoordinates, this.state.xPoint, this.state.yPoint);
     return(
       <View>
