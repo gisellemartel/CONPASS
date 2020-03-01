b160a2360f68050213f748200856d4d1558b2802
Giselle Martel <gmartel90@gmail.com>
2020-03-01 14:36:31 -0500

    Remove unecessary testing tet

--- components/withinBuilding/index.js
+++ components/withinBuilding/index.js
@@ -2,12 +2,8 @@
 /* eslint-disable no-plusplus */
 import React, { Component } from 'react';
 import {
-<<<<<<< HEAD
-  View, Text,
-=======
   View,
   Text,
->>>>>>> 1cf70f139f050f1d462366d6a361444bea47124d
   StyleSheet
 } from 'react-native';
 import * as Location from 'expo-location';
@@ -20,19 +16,11 @@ export default class WithinBuilding extends Component {
 
     this.state = {
       // user's current location
-<<<<<<< HEAD
-      location: '',
-
-      // to display information
-      fianlCampus: '',
-      fianlBuilding: '',
-=======
       location: {},
 
       // to display information
       campusDisplayName: '',
       buildingDisplayName: '',
->>>>>>> 1cf70f139f050f1d462366d6a361444bea47124d
 
       // polygon covering all buildings in SGW
       xSGWCoordinates: [-73.5866750, -73.5764890, -73.5715300, -73.5814433],
@@ -42,15 +30,9 @@ export default class WithinBuilding extends Component {
       xLOYCoordinates: [-73.6465040, -73.6396194, -73.6330318, -73.6428850],
       yLOYCoordinates: [45.4572870, 45.4548085, 45.4581882, 45.4619310],
 
-<<<<<<< HEAD
-      // SGW buildings formatted for pnpoly use
-      sgwBuildings: this.formatPolygonsObjs('SGW'),
-      // Loyola buildings formatted for pnpoly use
-=======
       // SGW buildings formatted for isInPolygon use
       sgwBuildings: this.formatPolygonsObjs('SGW'),
       // Loyola buildings formatted for isInPolygon use
->>>>>>> 1cf70f139f050f1d462366d6a361444bea47124d
       loyBuildings: this.formatPolygonsObjs('LOY')
     };
   }
@@ -59,65 +41,15 @@ export default class WithinBuilding extends Component {
   async getCurrentLocation() {
     const { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status !== 'granted') {
-<<<<<<< HEAD
-      console.error('Permission to access location was denied');
-=======
       this.setState({
         errorMessage: 'Permission to access location was denied',
       });
->>>>>>> 1cf70f139f050f1d462366d6a361444bea47124d
       return;
     }
     const location = await Location.getCurrentPositionAsync({});
     this.setState({ location });
   }
 
-<<<<<<< HEAD
-  // Format SGW or Loyola buildings to be used by pnpoly function
-  formatPolygonsObjs(campus) {
-    if (campus !== 'SGW' && campus !== 'LOY') {
-      return [];
-    }
-
-    const formattedBuildings = [];
-
-    const formattedBuildingsTemp = buildings.filter((building) => {
-      return building.campus === campus;
-    });
-
-    formattedBuildingsTemp.forEach((building) => {
-      const xCoordinates = [];
-      const yCoordinates = [];
-
-      (building.polygons[0].coordinates).forEach((pairOfCoordinates) => {
-        xCoordinates.push(pairOfCoordinates.longitude);
-        yCoordinates.push(pairOfCoordinates.latitude);
-      });
-
-      formattedBuildings.push({
-        name: building.buildingName,
-        xCoords: xCoordinates,
-        yCoords: yCoordinates
-      });
-    });
-
-    return formattedBuildings;
-  }
-
-  // finds if a pair of coordinates are inside a polygon
-  pnpoly(nvert, vertx, verty, testx, testy) {
-    let i;
-    let j;
-    let c = false;
-
-    for (i = 0, j = nvert - 1; i < nvert; j = i + 1) {
-      if (((verty[i] > testy) !== (verty[j] > testy))
-            // eslint-disable-next-line max-len
-            && ((testx < (vertx[j] - vertx[i]) * (testy - verty[i])) / (verty[j] - verty[i]) + vertx[i])) {
-        c = !c;
-      }
-    }
-=======
   // Format SGW or Loyola buildings to be used by isInPolygon function
   formatPolygonsObjs(campus) {
     if (campus !== 'SGW' && campus !== 'LOY') {
@@ -161,7 +93,6 @@ export default class WithinBuilding extends Component {
       if (((verty[i] > testy) !== (verty[j] > testy))
             && (testx < (vertx[j] - vertx[i]) * (testy - verty[i]) / (verty[j] - verty[i]) + vertx[i])) { c = !c; }
     }
->>>>>>> 1cf70f139f050f1d462366d6a361444bea47124d
     return c;
   }
 
@@ -170,37 +101,6 @@ export default class WithinBuilding extends Component {
     await this.getCurrentLocation();
 
     const { location } = this.state;
-<<<<<<< HEAD
-    const x = location.coords.longitude;
-    const y = location.coords.latitude;
-    const { xSGWCoordinates } = this.state.xSGWCoordinates;
-    const { ySGWCoordinates } = this.state.ySGWCoordinates;
-    const { xLOYCoordinates } = this.state.xLOYCoordinates;
-    const { yLOYCoordinates } = this.state.yLOYCoordinates;
-
-    if (this.pnpoly((xSGWCoordinates).length, xSGWCoordinates, ySGWCoordinates, x, y)) {
-      this.state.fianlCampus = 'SGW';
-      this.state.fianlBuilding = '';
-      (this.state.sgwBuildings).forEach((building) => {
-        if (this.pnpoly((building.xCoords).length, building.xCoords, building.yCoords, x, y)) {
-          this.state.fianlBuilding = building.name;
-          this.state.fianlCampus = 'SGW';
-        }
-      });
-    } else if (this.pnpoly((xLOYCoordinates).length, xLOYCoordinates, yLOYCoordinates, x, y)) {
-      this.state.fianlCampus = 'Loyola';
-      this.state.fianlBuilding = '';
-
-      (this.state.loyBuildings).forEach((building) => {
-        if (this.pnpoly((building.xCoords).length, building.xCoords, building.yCoords, x, y)) {
-          this.state.fianlBuilding = building.name;
-        }
-        this.state.fianlCampus = 'Loyola';
-      });
-    } else {
-      this.state.fianlCampus = '';
-      this.state.fianlBuilding = '';
-=======
 
     const x = location.coords.longitude;
     const y = location.coords.latitude;
@@ -222,9 +122,8 @@ export default class WithinBuilding extends Component {
         this.state.campusDisplayName = 'Loyola';
       });
     } else {
-      this.state.campusDisplayName = 'NONE';
-      this.state.buildingDisplayName = 'NONE';
->>>>>>> 1cf70f139f050f1d462366d6a361444bea47124d
+      this.state.campusDisplayName = '';
+      this.state.buildingDisplayName = '';
     }
   }
 
