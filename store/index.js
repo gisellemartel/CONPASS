f1da76c735a3c2ed1d94ecfe54cf1e6adf02229e
Nichita Hariton <nichitahariton@hotmail.com>
2020-03-05 20:45:17 -0500

    [#70] Remove language choice in menu

--- store/index.js
+++ store/index.js
@@ -5,13 +5,16 @@ const initialState = {
   language: 'en'
 };
 
-// Redux Reducer, this receives actions that are being DISPATCHED
+// Redux Reducer1, this receives actions that are being DISPATCHED
+// remove comment es-lint comment after setting action
+// eslint-disable-next-line no-unused-vars
 const reducer = (state = initialState, action) => {
-  if (action.type === 'CHANGE_LANGUAGE') {
-    return {
-      language: action.payload.language
-    };
-  }
+  // set action type here example:
+  // if (action.type === 'CHANGE_LANGUAGE') {
+  //   return {
+  //     language: action.payload.language
+  //   };
+  // }
   return state;
 };
 
