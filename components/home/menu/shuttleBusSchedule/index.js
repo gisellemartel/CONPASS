0503437a289af037d6f2d9f1fa9742d250a8092f
Trevor N <trevor.nagg@gmail.com>
2020-03-13 15:55:03 -0400

    [#66] Change the way the schedule information is loaded on the page in order to reduce code duplication

--- components/home/menu/shuttleBusSchedule/index.js
+++ components/home/menu/shuttleBusSchedule/index.js
@@ -6,84 +6,60 @@ import { ButtonGroup } from 'react-native-elements';
 import shuttleScheduleInformation from './shuttleScheduleService';
 import styles from './styles';
 
+const buttons = ['SGW', 'LOY'];
 export default class ShuttleSchedule extends Component {
   constructor() {
     super();
     this.state = {
-      selectedIndex: 0
+      selectedButtonIndex: 0
     };
-    this.updateIndex = this.updateIndex.bind(this);
+    this.updateButtonIndex = this.updateButtonIndex.bind(this);
   }
 
-  updateIndex(selectedIndex) {
-    this.setState({ selectedIndex });
+  getShuttleCampusInformation(selectedButtonIndex) {
+    if (selectedButtonIndex === 1) { return shuttleScheduleInformation.Mon_Thu.LOY; }
+    return shuttleScheduleInformation.Mon_Thu.SGW;
+  }
+
+  // Button index is as follows: 0 -> SGW and 1->LOY
+  updateButtonIndex(selectedButtonIndex) {
+    this.setState({ selectedButtonIndex });
   }
 
   render() {
-    const buttons = ['SGW', 'LOY'];
-    const { selectedIndex } = this.state;
+    const { selectedButtonIndex } = this.state;
     return (
       <View style={styles.container}>
         <ButtonGroup
-          onPress={this.updateIndex}
-          selectedIndex={selectedIndex}
+          onPress={this.updateButtonIndex}
+          selectedIndex={selectedButtonIndex}
           buttons={buttons}
           containerStyle={{ height: 25 }}
         />
-        {selectedIndex === 0
-          ? (
-            <ScrollView
-              horizontal
-              contentContainerStyle={{
-                flexGrow: 1,
-                width: '100%',
-              }}
-            >
-
-              <SectionList
-                sections={[
-                  {
-                    title: 'Sir George Williams Campus',
-                    data: shuttleScheduleInformation.Mon_Thu.SGW
-                  },
-                ]}
-                renderItem={({ item }) => { return <Text style={styles.item}>{item}</Text>; }}
-                renderSectionHeader={
+        <ScrollView
+          horizontal
+          contentContainerStyle={{
+            flexGrow: 1,
+            width: '100%',
+          }}
+        >
+          <SectionList
+            sections={[
+              {
+                title: selectedButtonIndex === 0
+                  ? 'Sir George Williams Campus' : 'Loyola Campus',
+                data: this.getShuttleCampusInformation(selectedButtonIndex)
+              },
+            ]}
+            renderItem={({ item }) => { return <Text style={styles.item}>{item}</Text>; }}
+            renderSectionHeader={
                     ({ section }) => {
                       return <Text style={styles.sectionHeader}>{section.title}</Text>;
                     }
-}
-                keyExtractor={(item, index) => { return index; }}
-              />
-
-            </ScrollView>
-          ) : (
-            <ScrollView
-              horizontal
-              contentContainerStyle={{
-                flexGrow: 1,
-                width: '100%',
-              }}
-            >
-
-              <SectionList
-                sections={[
-                  {
-                    title: 'Loyola Campus',
-                    data: shuttleScheduleInformation.Mon_Thu.LOY
-                  },
-                ]}
-                renderItem={({ item }) => { return <Text style={styles.item}>{item}</Text>; }}
-                renderSectionHeader={
-                    ({ section }) => {
-                      return <Text style={styles.sectionHeader}>{section.title}</Text>;
-                    }
-}
-                keyExtractor={(item, index) => { return index; }}
-              />
-
-            </ScrollView>
-          )}
+              }
+            keyExtractor={(item, index) => { return index; }}
+          />
+        </ScrollView>
       </View>
     );
   }
