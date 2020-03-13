a6637817842eb45d7ccc3e46a8d56d9de76e88e8
Trevor N <trevor.nagg@gmail.com>
2020-03-13 01:05:46 -0400

    [#66] Add toggle button to switch schedule views depending on the campus

--- components/home/menu/shuttleBusSchedule/index.js
+++ components/home/menu/shuttleBusSchedule/index.js
@@ -2,41 +2,88 @@ import React, { Component } from 'react';
 import {
   View, Text, ScrollView, SectionList
 } from 'react-native';
+import { ButtonGroup } from 'react-native-elements';
 import shuttleScheduleInformation from './shuttleScheduleService';
 import styles from './styles';
 
 export default class ShuttleSchedule extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedIndex: 0
+    };
+    this.updateIndex = this.updateIndex.bind(this);
+  }
+
+  updateIndex(selectedIndex) {
+    this.setState({ selectedIndex });
+  }
+
   render() {
+    const buttons = ['SGW', 'LOY'];
+    const { selectedIndex } = this.state;
     return (
       <View style={styles.container}>
-        <ScrollView
-          horizontal
-          contentContainerStyle={{
-            flexGrow: 1,
-            width: '100%',
-          }}
-        >
-          <SectionList
-            sections={[
-              {
-                title: 'Sir George Williams Campus',
-                data: shuttleScheduleInformation.Mon_Thu.SGW
-              },
-              {
-                title: 'Loyola Campus',
-                data: shuttleScheduleInformation.Mon_Thu.LOY
-              },
-            ]}
-            renderItem={({ item }) => { return <Text style={styles.item}>{item}</Text>; }}
-            renderSectionHeader={
+        <ButtonGroup
+          onPress={this.updateIndex}
+          selectedIndex={selectedIndex}
+          buttons={buttons}
+          containerStyle={{ height: 25 }}
+        />
+        {selectedIndex === 0
+          ? (
+            <ScrollView
+              horizontal
+              contentContainerStyle={{
+                flexGrow: 1,
+                width: '100%',
+              }}
+            >
+
+              <SectionList
+                sections={[
+                  {
+                    title: 'Sir George Williams Campus',
+                    data: shuttleScheduleInformation.Mon_Thu.SGW
+                  },
+                ]}
+                renderItem={({ item }) => { return <Text style={styles.item}>{item}</Text>; }}
+                renderSectionHeader={
                     ({ section }) => {
                       return <Text style={styles.sectionHeader}>{section.title}</Text>;
                     }
 }
-            keyExtractor={(item, index) => { return index; }}
-          />
+                keyExtractor={(item, index) => { return index; }}
+              />
 
-        </ScrollView>
+            </ScrollView>
+          ) : (
+            <ScrollView
+              horizontal
+              contentContainerStyle={{
+                flexGrow: 1,
+                width: '100%',
+              }}
+            >
+
+              <SectionList
+                sections={[
+                  {
+                    title: 'Loyola Campus',
+                    data: shuttleScheduleInformation.Mon_Thu.LOY
+                  },
+                ]}
+                renderItem={({ item }) => { return <Text style={styles.item}>{item}</Text>; }}
+                renderSectionHeader={
+                    ({ section }) => {
+                      return <Text style={styles.sectionHeader}>{section.title}</Text>;
+                    }
+}
+                keyExtractor={(item, index) => { return index; }}
+              />
+
+            </ScrollView>
+          )}
       </View>
     );
   }
