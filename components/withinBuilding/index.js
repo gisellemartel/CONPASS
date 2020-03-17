c2286bc83413eafb9580e530ac042e1442fe35ee
sebhani <hani-111-222@hotmail.com>
2020-03-17 02:58:52 -0400

    [#14] Update currentBuilding prop value within componentDidUpdate method

--- components/withinBuilding/index.js
+++ components/withinBuilding/index.js
@@ -150,6 +150,9 @@ export default class WithinBuilding extends Component {
     this.updateCurrentBuildingProp();
   }
 
+  componentDidUpdate(){
+    this.updateCurrentBuildingProp();
+  }
   render() {
     this.buildingName();
     //remove b4 merging... It's just for testing purposes
