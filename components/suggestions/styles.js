080fdb84d7806529d2e8424e2d8ede404c97a5fc
t98b <bazerbachi.talal@gmail.com>
2020-03-18 02:39:33 -0400

    [#12] Add styling to carousel

--- components/suggestions/styles.js
+++ components/suggestions/styles.js
@@ -11,6 +11,28 @@ const styles = StyleSheet.create({
     height: 350,
     position: 'absolute'
   },
+
+  carousel: {
+    bottom: 0,
+    marginBottom: 48,
+    borderRadius: 10
+  },
+
+  slide: {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    height: 200,
+    width: 300,
+    padding: 24,
+    borderRadius: 24
+  },
+
+  image: {
+    height: 150,
+    width: 300,
+    bottom: 0,
+    position: 'absolute',
+    borderRadius: 10
+  }
 });
 
 export default styles;
