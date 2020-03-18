433400c65e1251ab9632c42d44c1d9c5f17aeaa2
t98b <bazerbachi.talal@gmail.com>
2020-03-18 02:38:27 -0400

    [#12] Add styling to container

--- components/suggestions/styles.js
+++ components/suggestions/styles.js
@@ -1,7 +1,16 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-
+  container: {
+    backgroundColor: 'white',
+    zIndex: 1,
+    bottom: -1,
+    width: '100%',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    height: 350,
+    position: 'absolute'
+  },
 });
 
 export default styles;
