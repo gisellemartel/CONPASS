d8e44154a980c13c129506e293767be234ddb51c
GD <gindulink@gmail.com>
2020-03-18 19:51:22 -0400

    Add pill shape

--- components/switchCampuses/styles.js
+++ components/switchCampuses/styles.js
@@ -1,10 +1,23 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  btn: {
+  buttonLeft: {
     width: '30%',
-    bottom: '80%',
-    marginHorizontal: 5,
+    bottom: '90%',
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 0,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 0,
+    overflow: 'hidden',
+  },
+  buttonRight: {
+    width: '30%',
+    bottom: '90%',
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 20,
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 20,
+    overflow: 'hidden',
   },
   container: {
     flexDirection: 'row',
