10923d4485e73d13fb81374783d4068adae0c227
GD <gindulink@gmail.com>
2020-03-19 02:50:47 -0400

    [#65] add opacity to buttons

--- components/switchCampuses/styles.js
+++ components/switchCampuses/styles.js
@@ -1,29 +1,31 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   buttonLeft: {
-    width: '30%',
-    bottom: '90%',
+    width: "30%",
+    bottom: "90%",
     borderBottomLeftRadius: 20,
     borderBottomRightRadius: 0,
     borderTopLeftRadius: 20,
     borderTopRightRadius: 0,
-    overflow: 'hidden',
+    overflow: "hidden",
+    opacity: 0.6
   },
   buttonRight: {
-    width: '30%',
-    bottom: '90%',
+    width: "30%",
+    bottom: "90%",
     borderBottomLeftRadius: 0,
     borderBottomRightRadius: 20,
     borderTopLeftRadius: 0,
     borderTopRightRadius: 20,
-    overflow: 'hidden',
+    overflow: "hidden",
+    opacity: 0.6
   },
   container: {
-    flexDirection: 'row',
-    position: 'absolute',
-    justifyContent: 'center',
-  },
+    flexDirection: "row",
+    position: "absolute",
+    justifyContent: "center"
+  }
 });
 
 export default styles;
