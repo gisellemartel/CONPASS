f7cd6e317d3a7723c5c6a28afddbf645216be830
sebhani <hani-111-222@hotmail.com>
2020-03-22 17:55:31 -0400

    [#14] Fix merge master into US2B

--- components/home/index.js
+++ components/home/index.js
@@ -1,28 +1,26 @@
 /* eslint-disable react/no-unused-state */
-import React, { Component } from 'react';
-import { View } from 'react-native';
-import TheMap from '../map';
-import SearchBar from '../searchBar';
-import Location from '../location';
-import SwitchCampuses from '../switchCampuses';
-// TODO: uncomment once #93 is merged
-import WithinBuilding from '../withinBuilding';
-import SetPath from '../setPath';
-import Addresses from '../addresses';
-import styles from './styles';
+import React, { Component } from "react";
+import { View } from "react-native";
+import TheMap from "../map";
+import SearchBar from "../searchBar";
+import Location from "../location";
+import SwitchCampuses from "../switchCampuses";
+import SetPath from "../setPath";
+import Addresses from "../addresses";
+import styles from "./styles";
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
       // Set Initial region of the map
-      value: '',
+      value: "",
       coordinates: [],
       region: {
-        latitude: '',
-        longitude: '',
-        latitudeDelta: '',
-        longitudeDelta: ''
+        latitude: "",
+        longitude: "",
+        latitudeDelta: "",
+        longitudeDelta: ""
       },
       presetRegion: {
         latitude: 45.492408,
@@ -36,7 +34,7 @@ class Home extends Component {
       isGoVisible: false,
       isSwitchAvailableIndestination: true,
       // current Concordia a user is in
-      currentBuildingAddress: '',
+      currentBuildingAddress: "",
 
       showDirectionsMenu: false,
       showCampusToggle: false
@@ -44,10 +42,10 @@ class Home extends Component {
   }
 
   /**
-  * updates region and passes the new region 'map' component.
-  * @param {object} newRegion - New region to be passed.
-  */
-  updateRegion = (newRegion) => {
+   * updates region and passes the new region 'map' component.
+   * @param {object} newRegion - New region to be passed.
+   */
+  updateRegion = newRegion => {
     this.setState({
       presetRegion: {
         latitude: newRegion.latitude,
@@ -71,15 +69,15 @@ class Home extends Component {
    * destination option in directions mode
    * @param {object} destination - current selected destination
    */
-  getDestinationIfSet = (destination) => {
+  getDestinationIfSet = destination => {
     this.setState({ destinationToGo: destination });
-  }
+  };
 
   /**
    * Changes visibility of directions search menus depending on context
    * @param {*} showDirectionsMenu - desired visibility boolean
    */
-  changeVisibilityTo = (showDirectionsMenu) => {
+  changeVisibilityTo = showDirectionsMenu => {
     this.setState({
       showDirectionsMenu
     });
@@ -89,44 +87,43 @@ class Home extends Component {
    * Changes visibility of campus toggle when search bar is focused/blurred
    * @param {*} showCampusToggle - desired visibility boolean
    */
-  setCampusToggleVisibility = (showCampusToggle) => {
+  setCampusToggleVisibility = showCampusToggle => {
     this.setState({
       showCampusToggle
     });
-  }
+  };
 
   /**
-  * updates coordinates and passes new coordinates 'Map' component.
-  * @param {object} newCoordinates - New coordinates to be passed.
-  */
-  updateCoordinates = (newCoordinates) => {
+   * updates coordinates and passes new coordinates 'Map' component.
+   * @param {object} newCoordinates - New coordinates to be passed.
+   */
+  updateCoordinates = newCoordinates => {
     this.setState({
       coordinates: newCoordinates
     });
   };
 
   /**
-  * gets new region from 'Addresses' component and updates region state
-  * @param {object} region - New region to be passed.
-  */
-  getRegionFromAddresses=(region) => {
+   * gets new region from 'Addresses' component and updates region state
+   * @param {object} region - New region to be passed.
+   */
+  getRegionFromAddresses = region => {
     this.updateRegion(region);
   };
 
   /**
-  * gets new coordinates from 'Addresses' component and updates coordinates state
-  * @param {object} coordinates - New coordinates to be passed.
-  */
-  getCoordinatesFromAddresses=(coordinates) => {
+   * gets new coordinates from 'Addresses' component and updates coordinates state
+   * @param {object} coordinates - New coordinates to be passed.
+   */
+  getCoordinatesFromAddresses = coordinates => {
     this.updateCoordinates(coordinates);
-  }
+  };
 
-
-  updateCurrentBuildingAddress = (childCurrentBuilding) => {
+  updateCurrentBuildingAddress = childCurrentBuilding => {
     this.setState({
       currentBuildingAddress: childCurrentBuilding
     });
-  }
+  };
 
   render() {
     return (
@@ -137,39 +134,40 @@ class Home extends Component {
           polylineVisibility={this.state.showDirectionsMenu}
         />
         {!this.state.showDirectionsMenu && (
-        <SearchBar
-          getDestinationIfSet={this.getDestinationIfSet}
-          navigation={this.props.navigation}
-          updateRegion={this.updateRegion}
-          changeVisibilityTo={this.changeVisibilityTo}
-          setCampusToggleVisibility={this.setCampusToggleVisibility}
-          currentBuildingPred={this.state.currentBuildingAddress}
-        />
+          <SearchBar
+            getDestinationIfSet={this.getDestinationIfSet}
+            navigation={this.props.navigation}
+            updateRegion={this.updateRegion}
+            changeVisibilityTo={this.changeVisibilityTo}
+            setCampusToggleVisibility={this.setCampusToggleVisibility}
+            currentBuildingPred={this.state.currentBuildingAddress}
+          />
         )}
         {this.state.showCampusToggle && (
-        <SwitchCampuses
-          updateRegion={this.updateRegion}
-          visiblityState={!this.state.showDirectionsMenu}
-        />
+          <SwitchCampuses
+            updateRegion={this.updateRegion}
+            visiblityState={!this.state.showDirectionsMenu}
+          />
         )}
-        <Location updateRegion={this.updateRegion} />
-        <WithinBuilding updateCurrentBuildingCallBack={this.updateCurrentBuildingAddress} />
+        <Location
+          updateRegion={this.updateRegion}
+          updateCurrentBuildingCallBack={this.updateCurrentBuildingAddress}
+        />
         <SetPath
           changeVisibilityTo={this.changeVisibilityTo}
           newValue={this.state.value}
         />
-        {this.state.showDirectionsMenu
-        && (
-        <Addresses
-          getDestinationIfSet={this.state.destinationToGo}
-          getRegion={this.getRegionFromAddresses}
-          getRegionFromSearch={this.state.region}
-          getCoordinates={this.getCoordinatesFromAddresses}
-          changeVisibilityTo={this.changeVisibilityTo}
-          navigation={this.props.navigation}
-          currentBuildingPred={this.state.currentBuildingAddress}
-        />
-        ) }
+        {this.state.showDirectionsMenu && (
+          <Addresses
+            getDestinationIfSet={this.state.destinationToGo}
+            getRegion={this.getRegionFromAddresses}
+            getRegionFromSearch={this.state.region}
+            getCoordinates={this.getCoordinatesFromAddresses}
+            changeVisibilityTo={this.changeVisibilityTo}
+            navigation={this.props.navigation}
+            currentBuildingPred={this.state.currentBuildingAddress}
+          />
+        )}
       </View>
     );
   }
--- components/location/index.js
+++ components/location/index.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-mixed-operators */
 /* eslint-disable no-plusplus */
-import React, { Component } from 'react';
-import { View, Image, Text } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import getCurrentLocation from './LocationServices';
-import buildings from '../../assets/polygons/polygons';
-import styles from './styles';
-import locateMe from './locate-me.png';
+import React, { Component } from "react";
+import { View, Image, Text } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import getCurrentLocation from "./LocationServices";
+import buildings from "../../assets/polygons/polygons";
+import styles from "./styles";
+import locateMe from "./locate-me.png";
 
 export default class Location extends Component {
   constructor(props) {
@@ -14,52 +14,52 @@ export default class Location extends Component {
 
     this.state = {
       // to display information
-      campusDisplayName: '',
-      buildingDisplayName: '',
+      campusDisplayName: "",
+      buildingDisplayName: "",
 
       // polygon covering all buildings in SGW
-      xSGWCoordinates: [-73.5866750, -73.5764890, -73.5715300, -73.5814433],
-      ySGWCoordinates: [45.4963950, 45.4912630, 45.4961810, 45.5008758],
+      xSGWCoordinates: [-73.586675, -73.576489, -73.57153, -73.5814433],
+      ySGWCoordinates: [45.496395, 45.491263, 45.496181, 45.5008758],
 
       // polygon covering all buildings in Loyola
-      xLOYCoordinates: [-73.6465040, -73.6396194, -73.6330318, -73.6428850],
-      yLOYCoordinates: [45.4572870, 45.4548085, 45.4581882, 45.4619310],
+      xLOYCoordinates: [-73.646504, -73.6396194, -73.6330318, -73.642885],
+      yLOYCoordinates: [45.457287, 45.4548085, 45.4581882, 45.461931],
 
       // SGW buildings formatted for isInPolygon use
-      sgwBuildings: this.formatPolygonsObjs('SGW'),
+      sgwBuildings: this.formatPolygonsObjs("SGW"),
       // Loyola buildings formatted for isInPolygon use
-      loyBuildings: this.formatPolygonsObjs('LOY')
+      loyBuildings: this.formatPolygonsObjs("LOY")
     };
   }
-
 
   /** @param {string} campus - either 'SGW' or 'LOY'
    *  Format SGW or Loyola buildings to be used by isInPolygon function */
   formatPolygonsObjs(campus) {
-    if (campus !== 'SGW' && campus !== 'LOY') {
+    if (campus !== "SGW" && campus !== "LOY") {
       return [];
     }
 
     const formattedBuildings = [];
 
     // filter only the buildings that belong to the current campus
-    const formattedBuildingsTemp = buildings.filter((building) => {
+    const formattedBuildingsTemp = buildings.filter(building => {
       return building.campus === campus;
     });
 
-    formattedBuildingsTemp.forEach((building) => {
+    formattedBuildingsTemp.forEach(building => {
       const xCoordinates = [];
       const yCoordinates = [];
 
       const buildingPolygon = building.polygons[0];
 
-      (buildingPolygon.coordinates).forEach((pairOfCoordinates) => {
+      buildingPolygon.coordinates.forEach(pairOfCoordinates => {
         xCoordinates.push(pairOfCoordinates.longitude);
         yCoordinates.push(pairOfCoordinates.latitude);
       });
 
       formattedBuildings.push({
         name: building.buildingName,
+        address: building.address,
         xCoords: xCoordinates,
         yCoords: yCoordinates
       });
@@ -81,10 +81,14 @@ export default class Location extends Component {
     let j;
     let c = false;
     for (i = 0, j = nvert - 1; i < nvert; j = i++) {
-      if (((verty[i] > testy) !== (verty[j] > testy))
-            && (testx < (vertx[j] - vertx[i])
-            * (testy - verty[i])
-            / (verty[j] - verty[i]) + vertx[i])) { c = !c; }
+      if (
+        verty[i] > testy !== verty[j] > testy &&
+        testx <
+          ((vertx[j] - vertx[i]) * (testy - verty[i])) / (verty[j] - verty[i]) +
+            vertx[i]
+      ) {
+        c = !c;
+      }
     }
     return c;
   }
@@ -97,37 +101,105 @@ export default class Location extends Component {
 
     const x = this.state.region.longitude;
     const y = this.state.region.latitude;
-
-    if (this.isInPolygon((this.state.xSGWCoordinates).length,
-      this.state.xSGWCoordinates,
-      this.state.ySGWCoordinates, x, y)) {
-      this.state.campusDisplayName = 'SGW';
-      this.state.buildingDisplayName = '';
-      (this.state.sgwBuildings).forEach((sgwBuilding) => {
-        if (this.isInPolygon((sgwBuilding.xCoords).length,
-          sgwBuilding.xCoords,
-          sgwBuilding.yCoords, x, y)) {
-          this.state.buildingDisplayName = sgwBuilding.name;
-          this.state.campusDisplayName = 'SGW';
+    if (
+      this.isInPolygon(
+        this.state.xSGWCoordinates.length,
+        this.state.xSGWCoordinates,
+        this.state.ySGWCoordinates,
+        x,
+        y
+      )
+    ) {
+      this.setState({
+        campusDisplayName: "SGW",
+        buildingDisplayName: ""
+      });
+      this.state.sgwBuildings.forEach(sgwBuilding => {
+        if (
+          this.isInPolygon(
+            sgwBuilding.xCoords.length,
+            sgwBuilding.xCoords,
+            sgwBuilding.yCoords,
+            x,
+            y
+          )
+        ) {
+          this.setState({
+            buildingDisplayName: sgwBuilding.name,
+            campusDisplayName: "SGW"
+          });
         }
       });
-    } else if (this.isInPolygon((this.state.xLOYCoordinates).length,
-      this.state.xLOYCoordinates,
-      this.state.yLOYCoordinates, x, y)) {
-      this.state.campusDisplayName = 'Loyola';
-      this.state.buildingDisplayName = '';
-      (this.state.loyBuildings).forEach((loyBuilding) => {
-        if (this.isInPolygon((loyBuilding.xCoords).length,
-          loyBuilding.xCoords,
-          loyBuilding.yCoords, x, y)) { this.state.buildingDisplayName = loyBuilding.name; }
-        this.state.campusDisplayName = 'Loyola';
+    } else if (
+      this.isInPolygon(
+        this.state.xLOYCoordinates.length,
+        this.state.xLOYCoordinates,
+        this.state.yLOYCoordinates,
+        x,
+        y
+      )
+    ) {
+      this.setState({
+        campusDisplayName: "Loyola",
+        buildingDisplayName: ""
+      });
+      this.state.loyBuildings.forEach(loyBuilding => {
+        if (
+          this.isInPolygon(
+            loyBuilding.xCoords.length,
+            loyBuilding.xCoords,
+            loyBuilding.yCoords,
+            x,
+            y
+          )
+        ) {
+          this.setState({
+            buildingDisplayName: loyBuilding.name,
+            campusDisplayName: "Loyola"
+          });
+          //this.state.buildingDisplayName = loyBuilding.name;
+        }
+        //this.state.campusDisplayName = 'Loyola';
       });
     } else {
-      this.state.campusDisplayName = '';
-      this.state.buildingDisplayName = '';
+      this.setState({
+        campusDisplayName: "",
+        buildingDisplayName: ""
+      });
+      /*this.state.campusDisplayName = '';
+      this.state.buildingDisplayName = '';*/
     }
+
+    this.updateCurrentBuildingProp();
   }
 
+  /**
+   * Updates currentBuilding prop passed from Home component
+   */
+  updateCurrentBuildingProp() {
+    let buildingAddress = "";
+    if (
+      this.state.campusDisplayName === "Loyola" &&
+      this.state.buildingDisplayName.length > 0
+    ) {
+      buildingAddress = this.state.loyBuildings.filter(building => {
+        return building.name === this.state.buildingDisplayName;
+      })[0].address;
+      this.props.updateCurrentBuildingCallBack(buildingAddress);
+    } else if (
+      this.state.campusDisplayName === "SGW" &&
+      this.state.buildingDisplayName.length > 0
+    ) {
+      buildingAddress = this.state.sgwBuildings.filter(building => {
+        return building.name === this.state.buildingDisplayName;
+      })[0].address;
+      this.props.updateCurrentBuildingCallBack(buildingAddress);
+    } else {
+      this.props.updateCurrentBuildingCallBack(buildingAddress);
+    }
+
+    return buildingAddress;
+  }
   render() {
     return (
       <View style={styles.container}>
