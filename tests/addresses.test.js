6de4f04c644d0727d13c19b850012540423e04ad
h-karim <karimh96@hotmail.com>
2020-03-25 00:47:47 -0400

    [#17] add walking component test

--- tests/addresses.test.js
+++ tests/addresses.test.js
@@ -5,6 +5,7 @@ import Addresses from '../components/addresses';
 import Bus from '../components/addresses/bus';
 import Car from '../components/addresses/car';
 import Bike from '../components/addresses/bike';
+import Walking from '../components/addresses/walking';
 
 
 it('Should change mode of transportation to transit', () => {
@@ -32,4 +33,13 @@ it('Should change mode of transportation to bicycling', () => {
 
   bikeComponent.props.updateMode('bicycling');
   expect(addressesComponent.getInstance().state.mode).toBe('bicycling');
+});
+
+it('Should change mode of transportation to walking', () => {
+  const addressesComponent = renderer.create(<Addresses />);
+  instanceAddresses = addressesComponent.root;
+  const walkingComponent = instanceAddresses.findByType(Walking);
+
+  walkingComponent.props.updateMode('walking');
+  expect(addressesComponent.getInstance().state.mode).toBe('walking');
 });
