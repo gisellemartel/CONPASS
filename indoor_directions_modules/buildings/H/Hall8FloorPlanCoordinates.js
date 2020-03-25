71ebfc2718ee6314ccfcd76e39377cc26dedc675
Giselle Martel <gmartel90@gmail.com>
2020-03-25 04:22:22 -0400

    [#73] Add adjacency list for each intermediate graph node for Hall 8

--- indoor_directions_modules/buildings/H/Hall8FloorPlanCoordinates.js
+++ indoor_directions_modules/buildings/H/Hall8FloorPlanCoordinates.js
@@ -1,3 +1,9 @@
+/**
+ * Connected graph data structure
+ * representing rooms, points of interest,
+ * and paths of floor 8 in Hall Building
+ */
+
 const hall8FloorPlanCoordinates = {
   801: {
     x: 164,
@@ -456,238 +462,549 @@ const hall8FloorPlanCoordinates = {
   node_801: {
     x: 183,
     y: 215,
+    adjacencyList: [
+      '801',
+      'node_803',
+      'node_863'
+    ]
   },
   node_803: {
     x: 260,
     y: 215,
+    adjacencyList: [
+      '803',
+      'node_801',
+      'node_corner_805',
+      'staircase_1'
+    ]
   },
   node_805: {
     x: 334,
     y: 122,
+    adjacencyList: [
+      '805.03',
+      '805.02',
+      'node_corner_805',
+    ]
   },
   node_807: {
     x: 437,
     y: 215,
+    adjacencyList: [
+      '807',
+      'node_male_bathroom',
+      'node_north_intersection'
+    ]
   },
   node_809_01: {
     x: 535,
     y: 274,
+    adjacencyList: [
+      '809.01',
+      'node_north_intersection',
+      'node_809_02'
+    ]
   },
   node_809_02: {
     x: 535,
     y: 312,
+    adjacencyList: [
+      '809.02',
+      'node_809_01',
+      'node_809_03'
+    ]
   },
   node_809_03: {
     x: 535,
     y: 350,
+    adjacencyList: [
+      '809.03',
+      'node_809_02',
+      'node_escalator_corner_north'
+    ]
   },
   node_811: {
     x: 620,
     y: 215,
+    adjacencyList: [
+      '811',
+      'female_bathroom',
+      'node_female_bathroom',
+      'node_813'
+    ]
   },
   node_813: {
     x: 706,
     y: 215,
+    adjacencyList: [
+      '813',
+      'staircase_2',
+      'node_811',
+      'node_815'
+    ]
   },
   node_815: {
     x: 796,
     y: 215,
+    adjacencyList: [
+      '815',
+      'node_813',
+      'node_817',
+      'node_819'
+    ]
   },
   node_817: {
     x: 849,
     y: 172,
+    adjacencyList: [
+      '817',
+      'node_815',
+      'node_819'
+    ]
   },
   node_819: {
     x: 825,
     y: 243,
+    adjacencyList: [
+      '819',
+      'node_815',
+      'node_817',
+      'node_821'
+    ]
   },
   node_820: {
     x: 825,
     y: 466,
+    adjacencyList: [
+      '820',
+      'node_823',
+      'node_825'
+    ]
   },
   node_821: {
     x: 825,
     y: 333,
+    adjacencyList: [
+      '821',
+      'node_819',
+      'node_823'
+    ]
   },
   node_823: {
     x: 825,
     y: 426,
+    adjacencyList: [
+      '823',
+      'node_821',
+      'node_820'
+    ]
   },
   node_825: {
     x: 825,
     y: 515,
+    adjacencyList: [
+      '825',
+      'node_820',
+      'node_827'
+    ]
   },
   node_827: {
     x: 825,
     y: 605,
+    adjacencyList: [
+      '827',
+      '822',
+      'node_825',
+      'node_829'
+    ]
   },
   node_829: {
     x: 825,
     y: 744,
+    adjacencyList: [
+      '829',
+      'node_827',
+      'node_833'
+    ]
   },
   node_832_01: {
     x: 588,
     y: 800,
+    adjacencyList: [
+      '832.01',
+      'node_837',
+      'node_south_intersection'
+    ]
   },
   node_832_02_03: {
     x: 625,
     y: 720,
+    adjacencyList: [
+      '832.02',
+      '832.03',
+      'node_837',
+      'node_832_05_06'
+    ]
   },
   node_832_05_06: {
     x: 625,
     y: 686,
+    adjacencyList: [
+      '832.05',
+      '832.06',
+      'node_832_02_03'
+    ]
   },
   node_833: {
     x: 800,
     y: 800,
+    adjacencyList: [
+      '833',
+      'node_829',
+      'node_835'
+    ]
   },
   node_835: {
     x: 712,
     y: 800,
+    adjacencyList: [
+      '835',
+      'staircase_4',
+      'node_837',
+      'node_833'
+    ]
   },
   node_837: {
     x: 622,
     y: 800,
+    adjacencyList: [
+      '837',
+      'node_832_01',
+      'node_835',
+      'node_832_02_03'
+    ]
   },
   node_838_01: {
     x: 535,
     y: 687,
+    adjacencyList: [
+      '838.01',
+      'node_south_intersection',
+      'node_escalator_corner_south'
+    ]
   },
   node_838_841: {
     x: 438,
     y: 800,
+    adjacencyList: [
+      '838',
+      '841',
+      'node_corner_842',
+      'node_south_intersection',
+    ]
   },
   node_842: {
     x: 395,
     y: 691,
+    adjacencyList: [
+      '842',
+      'node_corner_842',
+    ]
   },
   node_843_881: {
     x: 348,
     y: 800,
+    adjacencyList: [
+      '843',
+      '881',
+      'node_corner_842',
+      'node_845',
+    ]
   },
   node_845: {
     x: 264,
     y: 800,
+    adjacencyList: [
+      '845',
+      'staircase_3',
+      'node_843_881',
+      'node_847'
+    ]
   },
   node_847: {
     x: 168,
     y: 801,
+    adjacencyList: [
+      '847',
+      'node_845',
+      'node_851',
+      'node_853'
+    ]
   },
   node_849: {
     x: 88,
     y: 809,
+    adjacencyList: [
+      '849',
+      '851.03',
+      '851.02',
+      'node_851'
+    ]
   },
   node_851: {
     x: 115,
     y: 809,
+    adjacencyList: [
+      '851.01',
+      'node_849',
+      'node_847'
+    ]
   },
   node_853: {
     x: 168,
     y: 694,
+    adjacencyList: [
+      '853',
+      '852',
+      'node_847',
+      'node_855'
+    ]
   },
   node_855: {
     x: 165,
     y: 600,
+    adjacencyList: [
+      '855',
+      '854',
+      '852',
+      'node_853',
+      'node_857'
+    ]
   },
   node_857: {
     x: 164,
     y: 512,
+    adjacencyList: [
+      '857',
+      'node_855',
+      'node_859'
+    ]
   },
   node_859: {
     x: 168,
     y: 425,
+    adjacencyList: [
+      '859',
+      'node_staircase_1',
+      'node_857',
+      'node_861'
+    ]
   },
   node_860_01: {
     x: 261,
-    y: 448
+    y: 448,
+    adjacencyList: [
+      '860.01',
+      'node_staircase_1',
+      'node_860_03_04'
+    ]
   },
   node_860_03_04: {
     x: 261,
-    y: 486
+    y: 486,
+    adjacencyList: [
+      '860.03',
+      '860.04',
+      'node_860_01',
+      'node_860_05_06'
+    ]
   },
   node_860_05_06: {
     x: 261,
-    y: 517
+    y: 517,
+    adjacencyList: [
+      '860.05',
+      '860.06',
+      'node_860_03_04'
+    ]
   },
   node_861: {
     x: 168,
     y: 324,
+    adjacencyList: [
+      '861',
+      'node_859',
+      'node_863'
+    ]
   },
   node_863: {
     x: 168,
     y: 241,
+    adjacencyList: [
+      '863',
+      'node_861',
+      'node_801',
+      'node_865_867'
+    ]
   },
   node_865_867: {
     x: 128,
     y: 174,
+    adjacencyList: [
+      '865',
+      '867',
+      'node_801',
+      'node_863'
+    ]
   },
   node_corner_805: {
     x: 335,
     y: 215,
+    adjacencyList: [
+      'node_805',
+      'node_803',
+      'node_male_bathroom'
+    ]
   },
   node_corner_842: {
     x: 395,
     y: 800,
+    adjacencyList: [
+      'node_842',
+      'node_843_881',
+      'node_838_841'
+    ]
   },
   node_elevator: {
     x: 347,
     y: 399,
+    adjacencyList: [
+      'elevator_1',
+      'elevator_2',
+      'node_staircase_1',
+      '862',
+      'node_accessible_bathroom'
+    ]
   },
   node_escalator_corner_north: {
     x: 535,
     y: 399,
+    adjacencyList: [
+      'node_escalator_down',
+      'node_809_03',
+      'node_escalator_corner_south'
+    ]
   },
   node_escalator_corner_south: {
     x: 535,
     y: 640,
+    adjacencyList: [
+      'node_escalator_up',
+      'node_escalator_corner_north',
+      'node_838.01'
+    ]
   },
   node_escalator_down: {
     x: 462,
     y: 399,
+    adjacencyList: [
+      'escalator',
+      'node_accessible_bathroom',
+      'node_escalator_corner_north',
+    ]
   },
   node_escalator_up: {
     x: 484,
     y: 640,
+    adjacencyList: [
+      'escalator',
+      'node_escalator_corner_south',
+    ]
   },
   node_accessible_bathroom: {
     x: 409,
-    y: 399
+    y: 399,
+    adjacencyList: [
+      'accessible_bathroom',
+      'node_elevator',
+      'node_escalator_down'
+    ]
   },
   node_female_bathroom: {
     x: 605,
     y: 215,
+    adjacencyList: [
+      'female_bathroom',
+      'node_811',
+      'node_north_intersection',
+    ]
   },
   node_male_bathroom: {
     x: 382,
     y: 215,
+    adjacencyList: [
+      'male_bathroom',
+      'node_807',
+      'node_corner_805',
+    ]
   },
   node_north_intersection: {
     x: 535,
     y: 215,
+    adjacencyList: [
+      'node_807',
+      'node_female_bathroom',
+      'node_809_01'
+    ]
   },
   node_south_intersection: {
     x: 535,
     y: 800,
+    adjacencyList: [
+      'node_832_01',
+      'node_838_01',
+      'node_838_841'
+    ]
   },
   node_staircase_1: {
     x: 261,
-    y: 399
+    y: 399,
+    adjacencyList: [
+      'staircase_1',
+      'node_elevator',
+      'node_860_01',
+      'node_859',
+      'node_861'
+    ]
   },
   staircase_1: {
     x: 214.99139,
     y: 277.79102,
+    adjacencyList: [
+      'node_staircase_1',
+      'node_803',
+    ]
   },
   staircase_2: {
     x: 685.38463,
     y: 274.84615,
+    adjacencyList: [
+      'node_813'
+    ]
   },
   staircase_3: {
     x: 270.96154,
     y: 686,
+    adjacencyList: [
+      'node_845'
+    ]
   },
   staircase_4: {
     x: 688.26925,
     y: 686.38462,
+    adjacencyList: [
+      'node_835'
+    ]
   },
 };
 
