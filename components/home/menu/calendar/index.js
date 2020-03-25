e1d312f9abdd6d3c857a3ce13f1982d936c217c8
sebhani <hani-111-222@hotmail.com>
2020-03-24 21:43:04 -0400

    [#27] Add navigation to calendar screen

---
+++ components/home/menu/calendar/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {
+  View, Text, Image, TouchableOpacity
+} from 'react-native';
+
+export default class Calendar extends Component {
+  render() {
+    return (
+      <View>
+        <Text>'Calendar'</Text>
+      </View>
+    );
+  }
+}
