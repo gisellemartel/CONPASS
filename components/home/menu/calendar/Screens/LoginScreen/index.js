e3e5bd18b8abcbdf079780aa77f89a5454266db4
t98b <bazerbachi.talal@gmail.com>
2020-03-26 14:42:33 -0400

    [#27] Add onSignIn method

--- components/home/menu/calendar/Screens/LoginScreen/index.js
+++ components/home/menu/calendar/Screens/LoginScreen/index.js
@@ -5,6 +5,33 @@ import * as Google from 'expo-google-app-auth';
 
 export default class LoginScreen extends Component {
 
+   onSignIn = (googleUser) => {
+  console.log('Google Auth Response', googleUser);
+  // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+  var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
+    unsubscribe();
+    // Check if we are already signed-in Firebase with the correct user.
+    if (!isUserEqual(googleUser, firebaseUser)) {
+      // Build Firebase credential with the Google ID token.
+      var credential = firebase.auth.GoogleAuthProvider.credential(
+          googleUser.getAuthResponse().id_token);
+      // Sign in with credential from the Google user.
+      firebase.auth().signInWithCredential(credential).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+    } else {
+      console.log('User already signed-in Firebase.');
+    }
+  });
+}
+
   signInWithGoogleAsync = async () => {
   try {
     const result = await Google.logInAsync({
