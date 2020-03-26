643ab2af67098f6f309ca9ea25dd2948c910ac9d
t98b <bazerbachi.talal@gmail.com>
2020-03-26 17:09:15 -0400

    [#27] Add Agenda

--- components/home/menu/calendar/Screens/DashboardScreen/index.js
+++ components/home/menu/calendar/Screens/DashboardScreen/index.js
@@ -6,47 +6,71 @@ import styles from './styles';
 export default class DashboardScreen extends Component {
   render() {
     return (
-      <View>
-        <Calendar
-  // Initially visible month. Default = Date()
-  current={'2012-03-01'}
+
+  <Agenda
+  // The list of items that have to be displayed in agenda. If you want to render item as empty date
+  // the value of date key has to be an empty array []. If there exists no value for date key it is
+  // considered that the date in question is not yet loaded
+  items={{
+    '2012-05-22': [{name: 'item 1 - any js object'}],
+    '2012-05-23': [{name: 'item 2 - any js object', height: 80}],
+    '2012-05-24': [],
+    '2012-05-25': [{name: 'item 3 - any js object'}, {name: 'any js object'}]
+  }}
+  // Callback that gets called when items for a certain month should be loaded (month became visible)
+  loadItemsForMonth={(month) => {console.log('trigger items loading')}}
+  // Callback that fires when the calendar is opened or closed
+  onCalendarToggled={(calendarOpened) => {console.log(calendarOpened)}}
+  // Callback that gets called on day press
+  onDayPress={(day)=>{console.log('day pressed')}}
+  // Callback that gets called when day changes while scrolling agenda list
+  onDayChange={(day)=>{console.log('day changed')}}
+  // Initially selected day
+  selected={'2012-05-16'}
   // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
   minDate={'2012-05-10'}
   // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
   maxDate={'2012-05-30'}
-  // Handler which gets executed on day press. Default = undefined
-  onDayPress={(day) => {console.log('selected day', day)}}
-  // Handler which gets executed on day long press. Default = undefined
-  onDayLongPress={(day) => {console.log('selected day', day)}}
-  // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-  monthFormat={'yyyy MM'}
-  // Handler which gets executed when visible month changes in calendar. Default = undefined
-  onMonthChange={(month) => {console.log('month changed', month)}}
-  // Hide month navigation arrows. Default = false
-  hideArrows={true}
-  // Replace default arrows with custom ones (direction can be 'left' or 'right')
-  renderArrow={(direction) => (<Arrow/>)}
-  // Do not show days of other months in month page. Default = false
-  hideExtraDays={true}
-  // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
-  // day from another month that is visible in calendar page. Default = false
-  disableMonthChange={true}
-  // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
-  firstDay={1}
-  // Hide day names. Default = false
-  hideDayNames={true}
-  // Show week numbers to the left. Default = false
-  showWeekNumbers={true}
-  // Handler which gets executed when press arrow icon left. It receive a callback can go back month
-  onPressArrowLeft={substractMonth => substractMonth()}
-  // Handler which gets executed when press arrow icon right. It receive a callback can go next month
-  onPressArrowRight={addMonth => addMonth()}
-  // Disable left arrow. Default = false
-  disableArrowLeft={true}
-  // Disable right arrow. Default = false
-  disableArrowRight={true}
+  // Max amount of months allowed to scroll to the past. Default = 50
+  pastScrollRange={50}
+  // Max amount of months allowed to scroll to the future. Default = 50
+  futureScrollRange={50}
+  // Specify how each item should be rendered in agenda
+  renderItem={(item, firstItemInDay) => {return (<View />);}}
+  // Specify how each date should be rendered. day can be undefined if the item is not first in that day.
+  renderDay={(day, item) => {return (<View />);}}
+  // Specify how empty date content with no items should be rendered
+  renderEmptyDate={() => {return (<View />);}}
+  // Specify how agenda knob should look like
+  // Specify what should be rendered instead of ActivityIndicator
+  renderEmptyData = {() => {return (<View />);}}
+  // Specify your item comparison function for increased performance
+  rowHasChanged={(r1, r2) => {return r1.text !== r2.text}}
+
+  // By default, agenda dates are marked if they have at least one item, but you can override this if needed
+  markedDates={{
+    '2012-05-16': {selected: true, marked: true},
+    '2012-05-17': {marked: true},
+    '2012-05-18': {disabled: true}
+  }}
+
+  // If provided, a standard RefreshControl will be added for "Pull to Refresh" functionality. Make sure to also set the refreshing prop correctly.
+  onRefresh={() => console.log('refreshing...')}
+  // Set this true while waiting for new data from a refresh
+  refreshing={false}
+  // Add a custom RefreshControl component, used to provide pull-to-refresh functionality for the ScrollView.
+  refreshControl={null}
+  // Agenda theme
+  theme={{
+
+    agendaDayTextColor: 'yellow',
+    agendaDayNumColor: 'green',
+    agendaTodayColor: 'red',
+    agendaKnobColor: '#84ECED'
+  }}
+
 />
-      </View>
+
     );
   }
 }
