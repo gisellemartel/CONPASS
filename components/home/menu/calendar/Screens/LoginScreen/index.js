335b5c00fb029ada724dee4ed09ea8ed747c17ad
t98b <bazerbachi.talal@gmail.com>
2020-03-26 14:37:58 -0400

    [#27] Sign In with google

--- components/home/menu/calendar/Screens/LoginScreen/index.js
+++ components/home/menu/calendar/Screens/LoginScreen/index.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import styles from './styles';
+import * as Google from 'expo-google-app-auth';
 
 export default class LoginScreen extends Component {
+
+  signInWithGoogleAsync = async () => {
+  try {
+    const result = await Google.logInAsync({
+      behavior: 'web',
+      androidClientId: '1074556967371-dq4p8r6ech6h3lobf9vvplm50276b5l7.apps.googleusercontent.com',
+      iosClientId: '1074556967371-dlgnaho4p5t17fjh3p6g364qqq70bh15.apps.googleusercontent.com',
+      scopes: ['profile', 'email'],
+    });
+
+    if (result.type === 'success') {
+      return result.accessToken;
+    } else {
+      return { cancelled: true };
+    }
+  } catch (e) {
+    return { error: true };
+  }
+};
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>LoginScreen</Text>
+        <Button
+          title="Sign In with Google"
+          onPress={() => { this.signInWithGoogleAsync(); }}
+        />
       </View>
     );
   }
