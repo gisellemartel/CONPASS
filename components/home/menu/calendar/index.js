3786d397482ed880c6b6b7f10ea74e21f7a69a2b
t98b <bazerbachi.talal@gmail.com>
2020-03-26 10:16:52 -0400

    [#27] Fix elinter issues

--- components/home/menu/calendar/index.js
+++ components/home/menu/calendar/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-//import apiCalendar from 'react-google-calendar-api';
+// import apiCalendar from 'react-google-calendar-api';
 import {
   View, Text, Button, TouchableOpacity
 } from 'react-native';
@@ -8,36 +8,36 @@ export default class Calendar extends Component {
   constructor(props){
     super(props);
   }
-//timeMin=2020-03-24T00:00:00.000Z
-  getEvents(){
-    return fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events?timeMin=2020-03-24T00:00:00.000Z', {
-      method: 'GET',  
-      headers: {
-          Authorization: 'Bearer ya29.a0Adw1xeWKP9dLAiDndQev1EJ6mps9BywoNOSAnwXZZ6nJ3KvS25y6ODOIevtV1cdDDJgMATabJ1fZw_qGfvvVduz6FKpzu4bH0DS5F6DAxUCirWGZgEsijOT0OWUw-CdEalLU0aCPnrYDJ62-1_0NWj9TyrGOX2s0NNY'
-        }
-      })
-      .then( (response) => response.json())
-      .then((responseJson) => {
-        console.log('--->',responseJson.items[3]);
-      })
 
-      .catch((error) =>{
-        console.log('-!!!!!!!!!!!!!!!!!!!!')
-        console.log(error)
-      });
-  }
-
-  //'2020-03-24T00:00:00+00:00'
-  componentDidMount(){
+// '2020-03-24T00:00:00+00:00'
+  componentDidMount() {
     this.getEvents();
   }
 
+// timeMin=2020-03-24T00:00:00.000Z
+  getEvents() {
+    return fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events?timeMin=2020-03-24T00:00:00.000Z', {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ya29.a0Adw1xeWKP9dLAiDndQev1EJ6mps9BywoNOSAnwXZZ6nJ3KvS25y6ODOIevtV1cdDDJgMATabJ1fZw_qGfvvVduz6FKpzu4bH0DS5F6DAxUCirWGZgEsijOT0OWUw-CdEalLU0aCPnrYDJ62-1_0NWj9TyrGOX2s0NNY'
+      }
+    })
+      .then((response) => { response.json(); })
+      .then((responseJson) => {
+        console.log('--->', responseJson.items[3]);
+      })
+
+      .catch((error) => {
+        console.log('-!!!!!!!!!!!!!!!!!!!!');
+        console.log(error);
+      });
+  }
+
   render() {
- 
     return (
       <View>
-        <Text>'Calendar'</Text>
-        <Button title='Sign In'/>
+        <Text>Calendar</Text>
+        <Button title="Sign In" />
       </View>
     );
   }
