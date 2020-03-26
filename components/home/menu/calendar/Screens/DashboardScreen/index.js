d744f013754284a54d61bb1dc20b76375e91e6a1
t98b <bazerbachi.talal@gmail.com>
2020-03-26 16:20:02 -0400

    [#27] Add calendar

--- components/home/menu/calendar/Screens/DashboardScreen/index.js
+++ components/home/menu/calendar/Screens/DashboardScreen/index.js
@@ -1,12 +1,51 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 import styles from './styles';
 
 export default class DashboardScreen extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>DashboardScreen</Text>
+      <View>
+        <Calendar
+  // Initially visible month. Default = Date()
+  current={'2012-03-01'}
+  // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+  minDate={'2012-05-10'}
+  // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+  maxDate={'2012-05-30'}
+  // Handler which gets executed on day press. Default = undefined
+  onDayPress={(day) => {console.log('selected day', day)}}
+  // Handler which gets executed on day long press. Default = undefined
+  onDayLongPress={(day) => {console.log('selected day', day)}}
+  // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+  monthFormat={'yyyy MM'}
+  // Handler which gets executed when visible month changes in calendar. Default = undefined
+  onMonthChange={(month) => {console.log('month changed', month)}}
+  // Hide month navigation arrows. Default = false
+  hideArrows={true}
+  // Replace default arrows with custom ones (direction can be 'left' or 'right')
+  renderArrow={(direction) => (<Arrow/>)}
+  // Do not show days of other months in month page. Default = false
+  hideExtraDays={true}
+  // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
+  // day from another month that is visible in calendar page. Default = false
+  disableMonthChange={true}
+  // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+  firstDay={1}
+  // Hide day names. Default = false
+  hideDayNames={true}
+  // Show week numbers to the left. Default = false
+  showWeekNumbers={true}
+  // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+  onPressArrowLeft={substractMonth => substractMonth()}
+  // Handler which gets executed when press arrow icon right. It receive a callback can go next month
+  onPressArrowRight={addMonth => addMonth()}
+  // Disable left arrow. Default = false
+  disableArrowLeft={true}
+  // Disable right arrow. Default = false
+  disableArrowRight={true}
+/>
       </View>
     );
   }
