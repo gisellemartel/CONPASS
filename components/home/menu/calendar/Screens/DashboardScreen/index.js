fdd06a36bbf92f8269c58d7d48923246cdd4e444
t98b <bazerbachi.talal@gmail.com>
2020-03-26 12:38:53 -0400

    [#27] Add text and style to dashboard screen

---
+++ components/home/menu/calendar/Screens/DashboardScreen/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+export default class DashboardScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>DashboardScreen</Text>
+      </View>
+    );
+  }
+}
