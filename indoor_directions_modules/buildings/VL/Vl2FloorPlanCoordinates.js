7fc276a2dadcdc42934e18b77de6ee95f2c24ca6
Giselle Martel <gmartel90@gmail.com>
2020-03-27 01:35:06 -0400

    [#73] Add path nodes for vl-2 svg

---
+++ indoor_directions_modules/buildings/VL/Vl2FloorPlanCoordinates.js
@@ -0,0 +1,155 @@
+/**
+ * Connected graph data structure
+ * representing rooms, points of interest,
+ * and paths of floor 2 in VL Building
+ */
+
+const vl2FloorPlanCoordinates = {
+  203: {
+    x: 650.666686,
+    y: 391.333351,
+  },
+  elevator: {
+    x: 199.999985,
+    y: 518,
+  },
+  female_bathroom: {
+    x: 650.000005,
+    y: 287.00001,
+  },
+  male_bathroom: {
+    x: 650.000005,
+    y: 204.00001,
+  },
+  node_203: {
+    x: 630,
+    y: 390,
+  },
+  node_center: {
+    x: 455,
+    y: 607,
+  },
+  node_east: {
+    x: 618,
+    y: 561,
+  },
+  node_elevator: {
+    x: 220.5,
+    y: 607.453125,
+  },
+  node_north: {
+    x: 512,
+    y: 362,
+  },
+  node_staircase_1: {
+    x: 736.5,
+    y: 815.453125,
+  },
+  node_staircase_2: {
+    x: 613,
+    y: 258,
+  },
+  node_staircase_3: {
+    x: 107.5,
+    y: 939.453125,
+  },
+  node_staircase_4: {
+    x: 613,
+    y: 277,
+  },
+  node_staircase_5: {
+    x: 253,
+    y: 493,
+  },
+  node_staircase_6: {
+    x: 629,
+    y: 353,
+  },
+  node_staircase_7: {
+    x: 234,
+    y: 354,
+  },
+  node_study_area_1: {
+    x: 138.5,
+    y: 615.453125,
+  },
+  node_study_area_2_1: {
+    x: 654.5,
+    y: 709.453125,
+  },
+  node_study_area_2_2: {
+    x: 653.5,
+    y: 752.453125,
+  },
+  node_study_area_3: {
+    x: 463.5,
+    y: 771.453125,
+  },
+  node_study_area_4: {
+    x: 208.5,
+    y: 910.453125,
+  },
+  node_study_area_5: {
+    x: 374,
+    y: 432,
+  },
+  node_study_area_6: {
+    x: 208.5,
+    y: 725.453125,
+  },
+  staircase_1: {
+    x: 755.999986,
+    y: 790.499999,
+  },
+  staircase_2: {
+    x: 548.999986,
+    y: 201.499999,
+  },
+  staircase_3: {
+    x: 51.999986,
+    y: 927.499999,
+  },
+  staircase_4: {
+    x: 548.999986,
+    y: 276.499999,
+  },
+  staircase_5: {
+    x: 206.999986,
+    y: 410.499999,
+  },
+  staircase_6: {
+    x: 725.999986,
+    y: 330.499999,
+  },
+  staircase_7: {
+    x: 206.999986,
+    y: 316.499999,
+  },
+  study_area_1: {
+    x: 82,
+    y: 597,
+  },
+  study_area_2: {
+    x: 612,
+    y: 730,
+  },
+  study_area_3: {
+    x: 412,
+    y: 803,
+  },
+  study_area_4: {
+    x: 158,
+    y: 941,
+  },
+  study_area_5: {
+    x: 326,
+    y: 403,
+  },
+  study_area_6: {
+    x: 82,
+    y: 730,
+  },
+};
+
+
+export default vl2FloorPlanCoordinates;
