e8a28e04b54e01e98d66fb8f3ae1efd4732687ec
Nichita Hariton <nichitahariton@hotmail.com>
2020-03-28 17:56:03 -0400

    Fix Calendar tests

--- tests/calendar.test.js
+++ tests/calendar.test.js
@@ -3,6 +3,10 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import DashboardScreen from '../components/home/menu/calendar/Screens/DashboardScreen';
 
+const navigation = {
+   state: { params: { events: { items: [{start: '1234', summary: '1234', end:'1234'}] } } } 
+}
+
 
 it('Should change rows', async () => {
   const params = {
@@ -10,7 +14,7 @@ it('Should change rows', async () => {
     rowHasChangedSecondParam: { name: 'y' }
   };
 
-  const dashboardScreenComponent = renderer.create(<DashboardScreen />).getInstance();
+  const dashboardScreenComponent = renderer.create(<DashboardScreen navigation={navigation} />).getInstance();
   const rowHasChanged = dashboardScreenComponent.rowHasChanged(params.rowHasChangedFirstParam, params.rowHasChangedSecondParam);
   expect(rowHasChanged).toBe(true);
 });
@@ -21,13 +25,13 @@ it('Shouldnt change rows', async () => {
     rowHasChangedSecondParam: { name: 'x' }
   };
 
-  const dashboardScreenComponent = renderer.create(<DashboardScreen />).getInstance();
+  const dashboardScreenComponent = renderer.create(<DashboardScreen navigation={navigation} />).getInstance();
   const rowHasChanged = dashboardScreenComponent.rowHasChanged(params.rowHasChangedFirstParam, params.rowHasChangedFirstParam);
   expect(rowHasChanged).toBe(false);
 });
 
 it('It should convert time to string', async () => {
-  const dashboardScreenComponent = renderer.create(<DashboardScreen />).getInstance();
+  const dashboardScreenComponent = renderer.create(<DashboardScreen navigation={navigation} />).getInstance();
   const dateFormatted = dashboardScreenComponent.timeToString(5);
   expect(dateFormatted).toStrictEqual('1970-01-01');
 });
