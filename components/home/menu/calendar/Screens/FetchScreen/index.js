217d987d4728d6e591e2f2c452a6213c68f33660
t98b <bazerbachi.talal@gmail.com>
2020-04-04 21:08:06 -0400

    [#29] signOut if access token is expired

--- components/home/menu/calendar/Screens/FetchScreen/index.js
+++ components/home/menu/calendar/Screens/FetchScreen/index.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import { View, AsyncStorage, ActivityIndicator } from 'react-native';
-
+import firebase from 'firebase';
 
 export default class FetchScreen extends Component {
   componentDidMount() {
+    this.checkToken();
     this.getData();
+  }
+
+  checkToken = async () => {
+    const accessToken = await AsyncStorage.getItem('accessToken');
+    const userInfoResponse = await fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events?key=AIzaSyBAHObp5Ic3CbJpkX2500tNhf53e_3wBMA&timeMin=2020-01-01T01:00:00.000Z', {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+    const jsonFile = await userInfoResponse.json();
+    const items = jsonFile;
+    if (!items) {
+      firebase.auth().signOut();
+      this.props.navigation.navigate('LoadingScreen');
+    }
   }
 
   getData = async () => {
