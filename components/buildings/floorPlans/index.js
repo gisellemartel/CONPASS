69dd596187167c26a60c01b4cf39cada9117634f
Giselle Martel <gmartel90@gmail.com>
2020-04-10 02:36:33 -0400

    [#22] Add new components for remaining floors of hall building

--- components/buildings/floorPlans/index.js
+++ components/buildings/floorPlans/index.js
@@ -1,5 +1,16 @@
 /* istanbul ignore file */
+export { default as Hall1 } from './H/Hall1';
+export { default as Hall2 } from './H/Hall2';
+export { default as Hall3 } from './H/Hall3';
+export { default as Hall4 } from './H/Hall4';
+export { default as Hall5 } from './H/Hall5';
+export { default as Hall6 } from './H/Hall6';
+export { default as Hall7 } from './H/Hall7';
 export { default as Hall8 } from './H/Hall8';
 export { default as Hall9 } from './H/Hall9';
+export { default as Hall10 } from './H/Hall10';
+export { default as Hall11 } from './H/Hall11';
+export { default as Hall12 } from './H/Hall12';
+export { default as Hall13 } from './H/Hall13';
 export { default as Vl1 } from './VL/Vl1';
 export { default as Vl2 } from './VL/Vl2';
