db04eb5c32b60844e5249c4d50458e7f4d0307e6
GD <gindulink@gmail.com>
2020-04-11 01:32:53 -0400

    [#139] Template for info

--- components/menu/help/index.js
+++ components/menu/help/index.js
@@ -1,23 +1,44 @@
 import React, { Component } from 'react';
 import {
-  View, Text, ScrollView, SectionList
+View, Text, Image, TouchableOpacity, Animated
 } from 'react-native';
-import { ButtonGroup } from 'react-native-elements';
+import HelpInformation from './helpInformation'
 import styles from './styles';
+import arrow from '../../../assets/icons/downarrow.png'
 
 const titles = ['Guide to CONPASS', 'Accessibility'];
 export default class Help extends Component{
-    constructor () {
-        super();
-        this.state = {
-            dropDownArrow: false
-        }
+constructor () {
+    super();
+    this.state = {
+        toggle: true
     }
+}
 
-    render() {
+
+render() {
     return (
-        <View style={styles.container}>
+    <View style={styles.container}>
+        <View style = {styles.pageHeader} >
+            <Text style = {{fontSize: 35}} > Topics </Text>
         </View>
-    );
-    }
+        <View style = {styles.topicElement}>
+            <Text style = {styles.TextStyle}> Guide to CONPASS </Text>
+            <TouchableOpacity>
+            <Image style={styles.arrowStyle} source={arrow} />
+            </TouchableOpacity>
+        </View>
+        <View style = {styles.topicElement}>
+            {/* Temp styling fix */ }
+            <Text style = {styles.TextStyle}> Accessibility {"            "} </Text>
+            <TouchableOpacity >
+            <Image style={styles.arrowStyle} source={arrow} />
+            </TouchableOpacity>
+        </View>
+        <View style={styles.body}>
+            {this.props.children}
+         </View>
+    </View>
+);
+}
 }
--- components/menu/help/styles.js
+++ components/menu/help/styles.js
@@ -1,11 +1,39 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    flex: 1,
-    backgroundColor: '#FFFFFF'
-  }
-  });
 
-  export default styles;
+container: {
+  width: '100%',
+  flex: 1,
+  flexDirection: 'column',
+  backgroundColor: '#F5FCFF'
+},
+pageHeader: {
+  flex: 1,
+  width: '100%',
+  fontWeight: 'bold',
+  top: 60,
+  marginLeft: 20,
+},
+topicElement: {
+  flex: 1,
+  width: '100%',
+  flexDirection: 'row',
+  padding : 10,
+  fontWeight:'bold',
+  marginLeft: 20,
+  bottom: 50,
+  backgroundColor: '#F5FCFF'
+},
+arrowStyle: {
+  width : 35,
+  height : 25,
+  left: 140,
+  },
+TextStyle: {
+  fontSize: 20,
+  textDecorationLine: 'underline',
+  }
+});
+
+export default styles;
