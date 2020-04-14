f72b61dfb33c0a9dc3e206affec0638e034c69e9
talal <bazerbachi.talal@gmail.com>
2020-04-14 02:17:41 -0400

    [#28] pass calendars to fetchScreen and render calendars in a list

--- components/menu/calendar/Screens/FetchScreen/index.js
+++ components/menu/calendar/Screens/FetchScreen/index.js
@@ -3,24 +3,31 @@ import { View, AsyncStorage, ActivityIndicator } from 'react-native';
 import CheckboxGroup from './CheckboxGroup';
 
 export default class FetchScreen extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-
     this.state={
-      calendarsGeneralInfo : [],
+      calendarsGeneralInfo :  [],
     };
     //this.getDatta=this.getDatta.bind(this);
   }
 
   componentDidMount() {
+        console.log('length:');
+    console.log(this.state.calendarsGeneralInfo.length);
     this.getDatta();
-    this.getData();
-  }
-  getDatta(){
-    if(this.props.navigation.state.params){
+    if (this.state.calendarsGeneralInfo.length == 0) {
       console.log('am here');
+       this.forceUpdate();
+    }
+    this.getData();
+
+  }
+
+
+  getDatta() {
+    if (this.props.navigation.state.params) {
       console.log(this.props.navigation.state.params.userCalendarsInfo);
-    this.setState({calendarsGeneralInfo: this.props.navigation.state.params.userCalendarsInfo});
+      this.setState({ calendarsGeneralInfo: this.props.navigation.state.params.userCalendarsInfo }, () => {this.forceUpdate();});
       console.log('after am here:');
       console.log(this.state.calendarsGeneralInfo);
     }
@@ -29,15 +36,16 @@ export default class FetchScreen extends Component {
   getData = async () => {
     const evnts = await AsyncStorage.getItem('events');
     const events = JSON.parse(evnts);
-    this.props.navigation.navigate('DashboardScreen', { events });
+  //this.props.navigation.navigate('DashboardScreen', { events });
   }
 
   render() {
     console.log('in render');
     console.log(this.state.calendarsGeneralInfo);
+
     return (
       <View>
-        <CheckboxGroup options={this.state.calendarsGeneralInfo}/>
+        <CheckboxGroup options={this.state.calendarsGeneralInfo} />
       </View>
     );
   }
