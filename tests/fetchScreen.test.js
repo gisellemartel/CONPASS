9d65e3ee7eea0e4b4fb62f6b77bdb646e42b29d7
sebhani <hani-111-222@hotmail.com>
2020-04-17 17:12:56 -0400

    [#28] Add test for getDatta function where undefined return is expect

--- tests/fetchScreen.test.js
+++ tests/fetchScreen.test.js
@@ -8,8 +8,14 @@ const userGeneralInfo = [{'id':'ev1','storageId':'evs1','summary':'Math A+'},
 {'id':'ev3','storageId':'evs3','summary':'Holidays'}];
 
 
-it('Should return an array with "events1" in first element', () => {
+it('Should return a react native component', () => {
     const fetchScreenComponent = renderer.create(<FetchScreen navigation={{state:{params:{userCalendarsInfo:userGeneralInfo}}}}/>).getInstance();
     const returnedComponent = fetchScreenComponent.getDatta();
     expect(returnedComponent.$$typeof.toString()).toBe('Symbol(react.element)');
+});
+
+it('Should return an undefined', () => {
+    const fetchScreenComponent = renderer.create(<FetchScreen navigation={{state:{params:null}}}/>).getInstance();
+    const returnedValue = fetchScreenComponent.getDatta();
+    expect(returnedValue).toBeUndefined();
 });
