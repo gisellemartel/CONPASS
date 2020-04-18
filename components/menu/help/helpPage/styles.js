5e9060cc5aaedbea08fb63bf6a4f789b6f31042e
GD <gindulink@gmail.com>
2020-04-18 19:03:37 -0400

    [#139] Add Android requirement for LayoutAnimation & clean up Title overlap

--- components/menu/help/helpPage/styles.js
+++ components/menu/help/helpPage/styles.js
@@ -1,17 +1,18 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#F5FCFF',
-      paddingTop: 20,
-    },
-    pageHeader: {
-      flex: 0.3 ,
-      width: '100%',
-      fontWeight: 'bold',
-      top: '4%',
-      marginLeft: '5%',
-    }
+  container: {
+    flex: 1,
+    paddingTop: 20,
+    backgroundColor: "#F5FCFF",
+  },
+  pageHeader: {
+    width: "100%",
+    fontWeight: "bold",
+    top: "4%",
+    marginLeft: "5%",
+    backgroundColor: "#F5FCFF", //backgroundColor: "#b6d836", //Light green-yelllow
+    marginBottom: 40,
+  },
 });
 export default styles;
--- components/menu/help/helpTemplate/styles.js
+++ components/menu/help/helpTemplate/styles.js
@@ -1,25 +1,26 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
-    width: '95%',
-    backgroundColor: '#F5FCFF',
-    margin:10,
-    overflow:'hidden'
-},
+    width: "95%",
+    backgroundColor: "#F5FCFF",
+    margin: 10,
+    overflow: "hidden",
+  },
   topicElement: {
-    flexDirection: 'row',
-    marginLeft: '5%',
-    marginRight: '10%'
-},
+    flexDirection: "row",
+    marginLeft: "5%",
+    marginRight: "10%",
+    backgroundColor: "#F5FCFF", //backgroundColor: "#a50da1", //Purple
+  },
   arrowStyle: {
-    width : 35,
-    height : 25,
+    width: 35,
+    height: 25,
   },
   TextStyle: {
     flex: 1,
     fontSize: 20,
-    textDecorationLine: 'underline',
+    textDecorationLine: "underline",
   },
 });
 
