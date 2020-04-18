70549166bffa4353f54e610c70ac2574d36478c9
sebhani <hani-111-222@hotmail.com>
2020-04-17 20:56:36 -0400

    [#28] Fix Linter issues for syncMultipleCalendars.test

--- tests/syncMultipleCalendars.test.js
+++ tests/syncMultipleCalendars.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { AsyncStorage } from 'react-native';
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
 import CheckboxGroup from '../components/menu/calendar/Screens/FetchScreen/CheckboxGroup';
 
 const calendarsGeneralInfo = [{
@@ -25,14 +24,18 @@ const calendarsGeneralInfo = [{
 
 
 it('Should return an array with "events1" in first element', () => {
-  const CheckboxGroupComponent = renderer.create(<CheckboxGroup options={calendarsGeneralInfo} />).getInstance();
+  const CheckboxGroupComponent = renderer.create(<CheckboxGroup
+    options={calendarsGeneralInfo}
+  />).getInstance();
   CheckboxGroupComponent.setState({ calendarsToSync: ['ev1'] });
   const calendarsSummary = CheckboxGroupComponent.getCalendarsToBeSynced();
   expect(calendarsSummary[0]).toBe('Holidays');
 });
 
 it('Should return an empty array', () => {
-  const CheckboxGroupComponent = renderer.create(<CheckboxGroup options={calendarsGeneralInfo} />).getInstance();
+  const CheckboxGroupComponent = renderer.create(<CheckboxGroup
+    options={calendarsGeneralInfo}
+  />).getInstance();
   CheckboxGroupComponent.setState({ calendarsToSync: [] });
   const calendarsSummary = CheckboxGroupComponent.getCalendarsToBeSynced();
   expect(calendarsSummary.length).toBe(0);
