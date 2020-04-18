cf729dbafe72842247203184d7a7c631734f6d4e
Mohamed <mohhef@gmail.com>
2020-04-18 17:09:22 -0400

    [#163] Rename polygons file in assets to building to match the constant name

--- components/location/index.js
+++ components/location/index.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import { View, Image, Text } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import getCurrentLocation from './LocationServices';
-import buildings from '../../assets/polygons/polygons';
+import buildings from '../../assets/polygons/buildings';
 import styles from './styles';
 import locateMe from '../../assets/icons/locate-me.png';
 
